bc1ef4793bb2ae019f70255ed4267418b7b35295
Sahidul Islam <jonyahmed19@gmail.com>
2022-06-22 16:23:59 +0600

    added Mern Project

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const obj = {
+    name: "Sahidul",
+    email: "hello@gmail.com",
+  };
+  res.json(obj);
+});
+
+app.listen(4000, () => {
+  console.log("Hello");
+});
