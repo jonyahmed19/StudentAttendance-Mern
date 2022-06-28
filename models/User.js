bd581c883724f1801dd5e3f4d0006d5a15b685bf
Sahidul Islam <jonyahmed19@gmail.com>
2022-06-28 08:38:55 +0600

    password hash & email validation

--- models/User.js
+++ models/User.js
@@ -1,8 +1,19 @@
 const { model, Schema } = require("mongoose");
 
 const userSchema = new Schema({
-  name: String,
-  email: String,
+  name: {
+    type: String,
+    minLength: 3,
+    maxLength: 15,
+  },
+  email: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+    },
+  },
   password: String,
   role: [String],
   accountStatus: String,
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
-
+const connectDB = require("./db");
+const User = require("./models/User");
+const bycrpt = require("bcrypt");
 const app = express();
+
+/**
+ *
+ * Body Parser Middleware
+ */
+
+app.use(express.json());
 
 const localLogger = (req, res, next) => {
   console.log(req.url, req.method);
@@ -8,10 +17,50 @@ const localLogger = (req, res, next) => {
   return res.json([{ works: "works" }]);
 };
 
+/**
+ * /register endpoint
+ * Name, Email, password
+ * password should be hash
+ */
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Fields must not be empty!" });
+  }
+  /**
+   * Checking email exists or not
+   */
+  let user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).json({ message: "User already exist" });
+  }
+
+  user = new User({
+    name,
+    email,
+    password,
+  });
+
+  const salt = await bycrpt.genSalt(10);
+  const hash = await bycrpt.hash(password, salt);
+  user.password = hash;
+
+  await user.save();
+  return res.status(201).json({ message: "User registered successfully" });
+});
+
 app.get("/", localLogger, (req, res, next) => {
   res.send("<h2>local Servers</h2>");
 });
 
-app.listen(4000, () => {
-  console.log("App is running");
-});
+connectDB("mongodb://localhost:27017/attendance-db")
+  .then(() => {
+    console.log("Database is connected");
+
+    app.listen(4000, () => {
+      console.log("App is running");
+    });
+  })
+  .catch((e) => console.log(e));
