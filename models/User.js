ac80125a9cb837b22b57e976843c88b2bfe0a9f5
Sahidul Islam <jonyahmed19@gmail.com>
2022-06-28 08:46:47 +0600

    Validation is done

--- models/User.js
+++ models/User.js
@@ -5,18 +5,34 @@ const userSchema = new Schema({
     type: String,
     minLength: 3,
     maxLength: 15,
+    required: true,
   },
   email: {
     type: String,
+    required: true,
     validate: {
       validator: function (v) {
         return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
       },
     },
   },
-  password: String,
-  role: [String],
-  accountStatus: String,
+  password: {
+    type: String,
+    minLength: [3, "Password is too short"],
+    maxLength: [30, "Password is too long"],
+    required: true,
+  },
+  role: {
+    type: [String],
+    required: true,
+    default: ["STUDENT"],
+  },
+  accountStatus: {
+    type: String,
+    enum: ["PENDING", "ACTIVE", "REJECTED"],
+    required: true,
+    default: "PENDING",
+  },
 });
 
 const User = model("User", userSchema);
