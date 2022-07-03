864335db84bb15ef52433dcbf4f5173406f4122f
Sahidul Islam <jonyahmed19@gmail.com>
2022-07-03 15:56:26 +0600

    service auth error fixed

--- service/auth.js
+++ service/auth.js
@@ -2,42 +2,41 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const registerServie = () => {
-     /**
-     * Checking email exists or not
-     */
-      let user = await User.findOne({ email });
-      if (user) {
-        return res.status(400).json({ message: "User already exist" });
-      }
-  
-      user = new User({
-        name,
-        email,
-        password,
-      });
-  
-      const salt = await bcrypt.genSalt(10);
-      const hash = await bcrypt.hash(password, salt);
-      user.password = hash;
-  
-      await user.save();
+const registerServie = async () => {
+  /**
+   * Checking email exists or not
+   */
+  let user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).json({ message: "User already exist" });
+  }
+
+  user = new User({
+    name,
+    email,
+    password,
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  user.password = hash;
+
+  await user.save();
 };
-const loginServie = () => {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: "Invalid credential" });
-    }
+const loginServie = async () => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credential" });
+  }
 
-    const isValidPassowrd = await bcrypt.compare(password, user.password);
+  const isValidPassowrd = await bcrypt.compare(password, user.password);
 
-    if (!isValidPassowrd) {
-      return res.status(400).json({ message: "Invalid credential" });
-    }
+  if (!isValidPassowrd) {
+    return res.status(400).json({ message: "Invalid credential" });
+  }
 
-    delete user._doc.password;
-    const token = jwt.sign(user?._doc, "jony", { expiresIn: "2h" });
-    
+  delete user._doc.password;
+  const token = jwt.sign(user?._doc, "jony", { expiresIn: "2h" });
 };
 
 module.exports = {
